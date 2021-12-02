8a5e26e6e090ae84020908179c3bd14d80ac5d3b
Rafsanjani Aziz <rafsanjani.aziz@sky.uk>
2021-12-02 09:08:23 +0000

    Day two

---
+++ src/Day02.kt
@@ -0,0 +1,67 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var horizontalPosition = 0
+        var depth = 0
+
+        input.map {
+            val (a, b) = it.split(" ")
+            Movement(a, b.toInt())
+        }
+            .forEach { movement ->
+                if (movement.direction == "forward") {
+                    horizontalPosition += movement.distance
+                }
+                if (movement.direction == "down") {
+                    depth += movement.distance
+                }
+                if (movement.direction == "up") {
+                    depth -= movement.distance
+                }
+            }.also {
+                println(horizontalPosition * depth)
+            }
+
+        return horizontalPosition * depth
+    }
+
+
+    fun part2(input: List<String>): Int{
+        var horizontalPosition = 0
+        var aim = 0
+        var depth = 0
+
+        input.map {
+            val (a, b) = it.split(" ")
+            Movement(a, b.toInt(), 0)
+        }.forEach { movement ->
+            if (movement.direction == "forward") {
+                horizontalPosition += movement.distance
+                if (aim != 0) {
+                    depth += movement.distance * aim
+                }
+            }
+
+            if (movement.direction == "down") {
+                aim += movement.distance
+            }
+            if (movement.direction == "up") {
+                aim -= movement.distance
+            }
+        }
+
+        return horizontalPosition * depth
+    }
+
+
+    check(part1(readInput("Day02")) == 1480518)
+    check(part1(readInput("Day02_Test")) == 150)
+
+    check(part2(readInput("Day02")) == 1282809906)
+    check(part2(readInput("Day02_Test")) == 900)
+}
+
+data class Movement(
+    val direction: String,
+    val distance: Int,
+    val aim: Int = 0
+)
