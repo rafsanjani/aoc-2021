94094622fe4529e6067da192ce9aa1a11f3bdd38
Rafsanjani Aziz <rafsanjani.aziz@sky.uk>
2021-12-04 12:23:47 +0000

    Day 3

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -3,69 +3,95 @@ package day03
 import readInput
 
 fun main() {
+
+    fun String.binaryToDecimal(): Int =
+        toInt(2)
+
+    fun String.gammaToEpsilon(): Int =
+        map { if (it == '0') '1' else '0' }
+            .joinToString(separator = "")
+            .toInt(2)
+
+
     fun part1(input: List<String>): Int {
-        var horizontalPosition = 0
-        var depth = 0
+        var gammaString = ""
 
-        input.map {
-            val (a, b) = it.split(" ")
-            Movement(a, b.toInt())
-        }
-            .forEach { movement ->
-                if (movement.direction == "forward") {
-                    horizontalPosition += movement.distance
-                }
-                if (movement.direction == "down") {
-                    depth += movement.distance
-                }
-                if (movement.direction == "up") {
-                    depth -= movement.distance
-                }
-            }.also {
-                println(horizontalPosition * depth)
+        val bitLength = input.first().length;
+
+        var position = 0;
+
+        while (position < bitLength) {
+            var binary = ""
+
+            for (item in input) {
+                binary += item[position]
             }
 
-        return horizontalPosition * depth
+            val zeroes = binary.count {
+                it == '0'
+            }
+
+            gammaString += if (zeroes > input.size / 2) {
+                "0"
+            } else {
+                "1"
+            }
+
+            position++
+        }
+
+
+        return gammaString.binaryToDecimal() * gammaString.gammaToEpsilon()
+    }
+
+    fun findOxygenRating(values: List<String>, position: Int = 0): String {
+        if (values.size == 1)
+            return values.first()
+
+        val highs = values.count { it[position] == '1' }
+        val lows = values.count { it[position] == '0' }
+
+
+        val bitToKeep = if (highs > lows) '1' else if (lows > highs) '0' else '1'
+
+        val newValues = mutableListOf<String>()
+
+        values.forEach {
+            if (it[position] == bitToKeep)
+                newValues.add(it)
+        }
+
+        return findOxygenRating(newValues, position + 1)
+    }
+
+    fun findCO2Rating(values: List<String>, position: Int = 0): String {
+        if (values.size == 1)
+            return values.first()
+
+        val highs = values.count { it[position] == '1' }
+        val lows = values.count { it[position] == '0' }
+
+
+        val bitToKeep = if (highs > lows) '0' else if (lows > highs) '1' else '0'
+
+        val newValues = mutableListOf<String>()
+
+        values.forEach {
+            if (it[position] == bitToKeep)
+                newValues.add(it)
+        }
+
+        return findCO2Rating(newValues, position + 1)
+    }
+
+    fun part2(input: List<String>): Int {
+        return findCO2Rating(input).binaryToDecimal() * findOxygenRating(input).binaryToDecimal()
     }
 
 
-    fun part2(input: List<String>): Int{
-        var horizontalPosition = 0
-        var aim = 0
-        var depth = 0
+    check(part1(readInput("day03/Day03_test")) == 198)
+    check(part1(readInput("day03/Day03")) == 3429254)
 
-        input.map {
-            val (a, b) = it.split(" ")
-            Movement(a, b.toInt(), 0)
-        }.forEach { movement ->
-            if (movement.direction == "forward") {
-                horizontalPosition += movement.distance
-                if (aim != 0) {
-                    depth += movement.distance * aim
-                }
-            }
-
-            if (movement.direction == "down") {
-                aim += movement.distance
-            }
-            if (movement.direction == "up") {
-                aim -= movement.distance
-            }
-        }
-
-        return horizontalPosition * depth
-    }
-
-
-    check(part1(readInput("Day02")) == 1480518)
-    check(part1(readInput("Day02_Test")) == 150)
-
-    check(part2(readInput("Day02")) == 1282809906)
-    check(part2(readInput("Day02_Test")) == 900)
+    check(part2(readInput("day03/Day03_test")) == 230)
+    check(part2(readInput("day03/Day03")) == 5410338)
 }
-
-data class Movement(
-    val direction: String,
-    val distance: Int,
-    val aim: Int = 0
-)
